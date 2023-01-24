f358429201140d4f92170828160c60e6bfe533a7
AlessandroDiNardo <alexdna003@gmail.com>
2023-01-24 17:10:18 +0100

    utilizzo for each per la stampa di più film

--- index.php
+++ index.php
@@ -7,34 +7,42 @@ class Movie {
     public $title;
     public $genre;
     public $year;
+    public $duration;
 
     // definito il costruttore
-    public function __construct($title, $genre, $year) {
+    public function __construct($title, $genre, $year, $duration) {
 
         $this -> title = $title;
         $this -> genre = $genre;
         $this -> year = $year;
+        $this -> duration = $duration;
     }
 
     public function getMovie() {
 
-       return "MOVIE: " . $this -> title 
-       . " / " . $this-> genre
-       . " / " . $this -> year; 
+       return "<strong> MOVIE: </strong>" . $this -> title 
+       . " / " . $this -> genre
+       . " / " . $this -> year
+       . " / " . $this -> duration
+       . "<br>" . "<br>";
     }
 }
 
 echo "<h1> MOVIE LIST </h1>";
 
-$movie1 = new Movie("Ritorno al futuro", "Fantascienza", "18 ottobre 1985");
-$movie2 = new Movie("Harry Potter e la pietra filosofale", "Fantascienza", "6 dicembre 2001");
+$movieList = [
+    new Movie("Ritorno al futuro", "Sci-fi/Commedia ", "18 ottobre 1985", "1h-56m"),
+    new Movie("Harry Potter e la pietra filosofale", "Cinema fantastico-Avventura", "6 dicembre 2001", "2h-32m"),
+    new Movie("Fast and Furious", "Giallo/Thriller", "21 settembre 2001", "1h-46m"),
+    new Movie("Avengers: Endgame", "Azione/Sci-fi", "24 aprile 2019", " 3h-2m"),
+    new Movie("Zoolander", "Commedia", "12 luglio 2002", "1h-29m"),
+];
 
-// var_dump($movie1);
+foreach ($movieList as $movie){
 
-echo $movie1 -> getMovie();
-    
-echo "<br>";
+    echo "<div>" . $movie -> getMovie() . "</div>";
+}
 
-echo $movie2 -> getMovie();
+
  
 
